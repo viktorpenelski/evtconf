9aadb73854cfd7999b6726d49ba715ef599ab1e3
byfo <vic.nqq@gmail.com>
2018-02-25 16:43:01 +0200

    webview now loads the correct content view upon creation

--- app/src/main/kotlin/byfo/evtconf/WebViewActivity.kt
+++ app/src/main/kotlin/byfo/evtconf/WebViewActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.webview.*
 
 /**
  * Created by Vic on 2/25/2018.
@@ -12,10 +13,12 @@ class WebViewActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.webview)
 
         //TODO(vic) webView needs to be scrollable.
         val webView = findViewById<WebView>(R.id.webview)
+
+        webview.settings.builtInZoomControls = true
 
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String) : Boolean {
