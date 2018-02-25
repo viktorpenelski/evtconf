e679ae9b0b424d587d7ce6681fe9daa9a6017890
byfo <victor.penelski@gmail.com>
2018-02-25 12:29:06 +0200

    extracted Entry to own class. Empty entries will no longer be used when displaying the list of events

---
+++ app/src/main/kotlin/byfo/evtconf/spreadsheet/Entry.kt
@@ -0,0 +1,42 @@
+package byfo.evtconf.spreadsheet
+
+import android.util.Log
+import org.json.JSONException
+
+/**
+ * Created by Viktor on 2/25/2018.
+ */
+data class Entry(val title: String, val time: String, val picture: String) {
+
+    /**
+     * an Entry is considered "empty" when either one of the elements that it cannot be used with -
+     * title or time, is an empty string.
+     *
+     * @return true if both title and time are empty strings.
+     */
+    fun isEmpty() : Boolean {
+        return title.isEmpty() && time.isEmpty()
+    }
+
+    companion object {
+
+        /**
+         * Serialize an Entry from given JSONObject.
+         * This JSONObject has to follow google sheet's API.
+         *
+         * For reference check out the following GET request, looking for the array element "feed.entry":
+         * https://spreadsheets.google.com/feeds/list/1_Ol_0bP-S3GqEXEGPL3ODKmHAdWBBXcOdE3_M4phVe0/1/public/values?alt=json
+         */
+        fun fromJSONObject(obj: org.json.JSONObject) : Entry {
+            return try {
+                Entry(
+                        obj.getJSONObject("gsx\$title").getString("\$t") ,
+                        obj.getJSONObject("gsx\$time").getString("\$t"),
+                        obj.getJSONObject("gsx\$picture").getString("\$t"))
+            } catch (e: JSONException) {
+                Log.e("EntrySerialization", "Something went wrong during deserialization of remote json - ${e.stackTrace}")
+                Entry("", "", "")
+            }
+        }
+    }
+}
--- app/src/main/kotlin/byfo/evtconf/spreadsheet/GetGoogleSpreadsheetTask.kt
+++ app/src/main/kotlin/byfo/evtconf/spreadsheet/GetGoogleSpreadsheetTask.kt
@@ -4,22 +4,23 @@ import android.R
 import android.content.Context
 import android.os.AsyncTask
 import android.util.Log
-import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import byfo.evtconf.MainActivity
 import org.json.JSONArray
 import org.json.JSONObject
 import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.web.client.RestTemplate
 import java.net.URL
 
+
 /**
  * Created by Vic on 2/24/2018.
  */
 
 class GetGoogleSpreadsheetTask(val context: Context, val view: ListView) : AsyncTask<URL, Int, List<Entry>>() {
+
+    private val TAG = "DownstreamTask"
+
 
     override fun doInBackground(vararg urls: URL): List<Entry>? {
 
@@ -31,44 +32,40 @@ class GetGoogleSpreadsheetTask(val context: Context, val view: ListView) : Async
         val response = restTemplate.getForObject(url, String::class.java)
 
         val jsonResponse = JSONObject(response)
-        Log.d("jsonObject", response.toString())
+        Log.d(TAG, response.toString())
 
         val jsonEntries = jsonResponse.getJSONObject("feed").getJSONArray("entry")
-        Log.d("allEntries", jsonEntries.toString())
 
         val entries = jsonEntries.iterator().asSequence().map {
-            it -> Entry(
-                it.getJSONObject("gsx\$title").getString("\$t") ,
-                it.getJSONObject("gsx\$time").getString("\$t"),
-                it.getJSONObject("gsx\$picture").getString("\$t"))
+            it -> Entry.fromJSONObject(it)
         }.toList()
 
-        Log.d("YAY", entries.toString())
+        Log.d(TAG, entries.toString())
 
         return entries
     }
 
     override fun onPostExecute(result: List<Entry>) {
 
-        val list = result.map {
-            it -> "${it.time} - ${it.title}"
-        }.toList()
+        val list = result
+                .filter { !it.isEmpty() }
+                .map { it -> "${it.time} - ${it.title}" }
+                .toList()
 
         val adapter = ArrayAdapter<String>(context, R.layout.simple_list_item_1, list)
         view.adapter = adapter
     }
 
     /**
-     * Custom iterator for JSONArray, mapping all entries as JSONObject.
+     * Custom iterator for JSONArray, mapping all elements to JSONObject.
      *
      * Since JSONArray does not have an iterator, a custom one has to be provided.
      * NOTE that mapping to JSONObject works here, because in the current payload, all elements
      * are JSONObjects, but it might not be the case for every JSONArray.
      */
-    private operator fun JSONArray.iterator(): Iterator<JSONObject>
-            = (0 until length()).asSequence()
+    private operator fun JSONArray.iterator(): Iterator<JSONObject> = (0 until length())
+            .asSequence()
             .map { get(it) as JSONObject }
             .iterator()
 }
 
-data class Entry(val title: String, val time: String, val picture: String)
